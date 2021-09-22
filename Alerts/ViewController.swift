a65a421618e244a4536c9535d19622b1b421ffaf
HELIO MARCUS NERY FIDALGO <hmnff@hotmail.com>
2021-09-22 16:47:45 +0100

    Alerts in iOS
    Aplicando Alerts em janelas e table view.

--- Alerts/ViewController.swift
+++ Alerts/ViewController.swift
@@ -9,6 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func abrirAlert(_ sender: Any) {
+        
+        let alerta = UIAlertController (title: "Adicionar Contato", message: "Deseja adicionar um Contato?", preferredStyle: .alert)
+        let confirmar = UIAlertAction (title: "Confirmar", style: .default, handler: nil)
+        let cancelar = UIAlertAction (title: "Cancelar", style: .destructive, handler: nil)
+        
+        alerta.addAction(confirmar)
+        alerta.addAction(cancelar)
+        
+        present(alerta, animated: true, completion: nil)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
